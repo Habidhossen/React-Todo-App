e27768d74fe8aa14dd57432a491e6ab6fb3fbf80
Habidhossen <habidhossen2@gmail.com>
2022-05-18 23:58:48 +0600

    code added

--- src/components/AllTodos.js
+++ src/components/AllTodos.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./Todo";
 
 const AllTodos = () => {
-  const dummyData = [
-    {
-      id: 1,
-      name: "Sleeping",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, necessitatibus!",
-    },
-    {
-      id: 2,
-      name: "playing",
-      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, necessitatibus!",
-    },
-  ];
+  const [todoList, setTodoList] = useState([]);
+  //   const dummyData = [
+  //     {
+  //       id: 1,
+  //       name: "Sleeping",
+  //       desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, necessitatibus!",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "playing",
+  //       desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, necessitatibus!",
+  //     },
+  //   ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const desc = event.target.desc.value;
+    const todo = { name: name, desc: desc };
+    const previous = [...todoList, todo];
+    setTodoList(previous);
+  };
+  //   console.log(todoList);
 
   return (
     <div>
@@ -21,11 +32,11 @@ const AllTodos = () => {
         <h1>React Todo App</h1>
       </div>
       <div>
-        <form className="add-todo-container">
-          <input type="text" placeholder="Task name" />
+        <form onSubmit={handleSubmit} className="add-todo-container">
+          <input name="name" type="text" placeholder="Task name" />
           <textarea
             placeholder="Description"
-            name=""
+            name="desc"
             id=""
             cols="30"
             rows="4"
@@ -36,9 +47,10 @@ const AllTodos = () => {
         </form>
       </div>
       <div>
-        {dummyData.map((singleTodo) => (
+        {/* {todoList.map((singleTodo) => (
           <Todo key={singleTodo.id} singleTodo={singleTodo} />
-        ))}
+        ))} */}
+        <Todo todoList={todoList} />
       </div>
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-const Todo = ({ singleTodo }) => {
-  const { name, desc } = singleTodo;
-
+const Todo = ({ todoList }) => {
   return (
-    <div className="todo-card">
-      <div>
-        <h1 className="card-title">{name}</h1>
-        <p className="card-desc">{desc}</p>
-      </div>
-      <div className="right-div">
-        <h4 className="delete-btn">X</h4>
-      </div>
+    <div>
+      {todoList.map((todo) => (
+        <>
+          <div className="todo-card">
+            <div>
+              <h1 className="card-title">{todo.name}</h1>
+              <p className="card-desc">{todo.desc}</p>
+            </div>
+            <div className="right-div">
+              <h4 className="delete-btn">X</h4>
+            </div>
+          </div>
+        </>
+      ))}
     </div>
   );
 };
